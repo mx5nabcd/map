74dedb928511205b87d8c59950f14de950f80135
DDokDDok1 <010wjdgns@gmail.com>
2024-11-11 16:26:45 +0900

    zz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.annotation:annotation:1.3.0")
 }
--- app/src/main/java/com/example/ddokddok/MainActivity.java
+++ app/src/main/java/com/example/ddokddok/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ddokddok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button imageButton =findViewById(R.id.button1);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, life_home.class);
+                startActivity(intent);
+            }
         });
     }
 }
