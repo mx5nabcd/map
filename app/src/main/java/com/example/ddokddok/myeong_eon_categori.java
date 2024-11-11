c55d283190d6859e529f505eabfd3b6697a7900b
DDokDDok1 <010wjdgns@gmail.com>
2024-11-11 16:55:21 +0900

    h

---
+++ app/src/main/java/com/example/ddokddok/myeong_eon_categori.java
@@ -0,0 +1,14 @@
+package com.example.ddokddok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class myeong_eon_categori extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myeong_eon_categori);
+    }
+}
