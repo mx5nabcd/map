ee37f102dfe893a0d7888db683112cad8a93cba9
DDokDDok1 <010wjdgns@gmail.com>
2024-11-18 15:36:45 +0900

    h

--- app/src/main/java/com/example/ddokddok/MainActivity.java
+++ app/src/main/java/com/example/ddokddok/MainActivity.java
@@ -7,9 +7,6 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         Button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, myeong_eon.class);
+                Intent intent = new Intent(MainActivity.this, myeong_eon_friend.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/ddokddok/myeong_eon_categori.java
+++ app/src/main/java/com/example/ddokddok/myeong_eon_categori.java
@@ -2,6 +2,9 @@ package com.example.ddokddok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class myeong_eon_categori extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myeong_eon_categori);
+
+        Button Button_friend =findViewById(R.id.button_categori6);
+
+        Button_friend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(myeong_eon_categori.this, myeong_eon_friend.class);
+                startActivity(intent);
+            }
+        });
     }
 }
