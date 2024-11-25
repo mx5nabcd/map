8732e4219c1ba58377fbc404b1e63af2fe5129f4
mx5nabcd <ogoj20815@naver.com>
2024-11-25 15:00:58 +0900

    1

--- app/src/main/java/com/example/ddokddok/quiz_categori.java
+++ app/src/main/java/com/example/ddokddok/quiz_categori.java
@@ -1,7 +1,9 @@
 package com.example.ddokddok;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -15,6 +17,17 @@ public class quiz_categori extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_categori);
+
+        Button Button_eng =findViewById(R.id.button_categori_english);
+
+
+        Button_eng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(quiz_categori.this, quiz_english.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
